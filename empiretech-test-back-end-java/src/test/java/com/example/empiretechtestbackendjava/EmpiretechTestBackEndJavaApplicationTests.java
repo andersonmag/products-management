1c170074db3957f7fdeb856fcfe257b13c092cef
Anderson <andersondel.dev@gmail.com>
2024-01-11 13:42:19 -0300

    initial project

---
+++ empiretech-test-back-end-java/src/test/java/com/example/empiretechtestbackendjava/EmpiretechTestBackEndJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.empiretechtestbackendjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpiretechTestBackEndJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
