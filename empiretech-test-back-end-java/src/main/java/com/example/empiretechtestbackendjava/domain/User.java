88fd112871c6a380f5f85757c180d7c65db28f7a
Anderson <andersondel.dev@gmail.com>
2024-01-15 00:27:34 -0300

    feat: Add User

---
+++ empiretech-test-back-end-java/src/main/java/com/example/empiretechtestbackendjava/domain/User.java
@@ -0,0 +1,23 @@
+package com.example.empiretechtestbackendjava.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@Getter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false, unique = true)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+}
