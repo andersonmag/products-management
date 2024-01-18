f44fae3acf9d558cda0bf6bc860b90434cd27ab3
Anderson <andersondel.dev@gmail.com>
2024-01-18 19:10:56 -0300

    feat: Add logout de usuario

--- empiretech-test-front-end-angular/src/app/app/page/header/header.component.ts
+++ empiretech-test-front-end-angular/src/app/app/page/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TokenAuthenticationService } from '../../service/token-authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+
+  constructor(private tokenService: TokenAuthenticationService, private router: Router) {}
+
+  logout() {
+    console.log("1ada")
+    this.tokenService.removeToken();
+    this.router.navigate(['/login']).then(() => window.location.reload());
+  }
 
 }
