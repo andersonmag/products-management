e7c5b2b54f82d15c989335ff4c48b0f5bd7fe68b
Anderson <andersondel.dev@gmail.com>
2025-03-29 15:58:43 -0300

    feat: Adiciona classes faltantes de tenant manage

---
+++ tenants-manage-java/src/main/java/com/github/andersonmag/tenantsmanagejava/TenantsManageJavaApplication.java
@@ -0,0 +1,13 @@
+package com.github.andersonmag.tenantsmanagejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenantsManageJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenantsManageJavaApplication.class, args);
+	}
+
+}
---
+++ tenants-manage-java/src/test/java/com/github/andersonmag/tenantsmanagejava/TenantsManageJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.andersonmag.tenantsmanagejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenantsManageJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
